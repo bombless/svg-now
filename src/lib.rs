c3b2a8e5d1de832629070e1677c156e595e6fe11
York Xiang <bombless@126.com>
2017-06-01 21:12:06 +0800

    Rustup according to `svg` crate

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@ pub fn render((width, height): (u64, u64), events: Vec<svg::SvgEvent>) -> Vec<u8
         canvas.extend_from_slice(&[255, 255, 255, 0])
     }
 
-    for svg::SvgEvent::Line { x1, x2, y1, y2, view_box } in events {
+    for svg::SvgEvent::Line { x1, x2, y1, y2, view_box, stroke, stroke_width } in events {
         let (re_x1, re_x2, re_y1, re_y2);
+        let avg;
         {
             let width = view_box[2] - view_box[0];
             let height = view_box[3] - view_box[1];
@@ -17,6 +18,7 @@ pub fn render((width, height): (u64, u64), events: Vec<svg::SvgEvent>) -> Vec<u8
             re_x2 = (x2 - view_box[0]) / width;
             re_y1 = (y1 - view_box[1]) / height;
             re_y2 = (y2 - view_box[1]) / height;
+            avg = f64::sqrt(width * height);
         }
 
         for offset in 0 .. width * height {
@@ -28,10 +30,10 @@ pub fn render((width, height): (u64, u64), events: Vec<svg::SvgEvent>) -> Vec<u8
 
             use distance::hit;
 
-            if hit((re_x1, re_y1, re_x2, re_y2), re_x, re_y) {
-                for i in 0 .. 3 {
-                    canvas[4 * offset as usize + i] = 0
-                }
+            if hit(stroke_width / avg / 2.0, (re_x1, re_y1, re_x2, re_y2), re_x, re_y) {
+                canvas[4 * offset as usize + 0] = stroke.red;
+                canvas[4 * offset as usize + 1] = stroke.green;
+                canvas[4 * offset as usize + 2] = stroke.blue;
 				canvas[4 * offset as usize + 3] = 255
             }
         }
