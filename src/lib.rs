4b6a968944e983cf2ff6574c61e3202f825dcac3
York Xiang <bombless@126.com>
2017-06-02 20:52:12 +0800

    Add support for `<text>`

--- src/lib.rs
+++ src/lib.rs
@@ -8,34 +8,39 @@ pub fn render((width, height): (u64, u64), events: Vec<svg::SvgEvent>) -> Vec<u8
         canvas.extend_from_slice(&[255, 255, 255, 0])
     }
 
-    for svg::SvgEvent::Line { x1, x2, y1, y2, view_box, stroke, stroke_width } in events {
-        let (re_x1, re_x2, re_y1, re_y2);
-        let avg;
-        {
-            let width = view_box[2] - view_box[0];
-            let height = view_box[3] - view_box[1];
-            re_x1 = (x1 - view_box[0]) / width;
-            re_x2 = (x2 - view_box[0]) / width;
-            re_y1 = (y1 - view_box[1]) / height;
-            re_y2 = (y2 - view_box[1]) / height;
-            avg = f64::sqrt(width * height);
-        }
+    for e in events {
+        match e {
+            svg::SvgEvent::Line { x1, x2, y1, y2, view_box, stroke, stroke_width } => {
+                let (re_x1, re_x2, re_y1, re_y2);
+                let avg;
+                {
+                    let width = view_box[2] - view_box[0];
+                    let height = view_box[3] - view_box[1];
+                    re_x1 = (x1 - view_box[0]) / width;
+                    re_x2 = (x2 - view_box[0]) / width;
+                    re_y1 = (y1 - view_box[1]) / height;
+                    re_y2 = (y2 - view_box[1]) / height;
+                    avg = f64::sqrt(width * height);
+                }
 
-        for offset in 0 .. width * height {
-            let x = offset % height;
-            let y = offset / height;
+                for offset in 0 .. width * height {
+                    let x = offset % height;
+                    let y = offset / height;
 
-            let re_x = x as f64 / width as f64;
-            let re_y = y as f64 / height as f64;
+                    let re_x = x as f64 / width as f64;
+                    let re_y = y as f64 / height as f64;
 
-            use distance::hit;
+                    use distance::hit;
 
-            if hit(stroke_width / avg / 2.0, (re_x1, re_y1, re_x2, re_y2), re_x, re_y) {
-                canvas[4 * offset as usize + 0] = stroke.red;
-                canvas[4 * offset as usize + 1] = stroke.green;
-                canvas[4 * offset as usize + 2] = stroke.blue;
-				canvas[4 * offset as usize + 3] = 255
-            }
+                    if hit(stroke_width / avg / 2.0, (re_x1, re_y1, re_x2, re_y2), re_x, re_y) {
+                        canvas[4 * offset as usize + 0] = stroke.red;
+                        canvas[4 * offset as usize + 1] = stroke.green;
+                        canvas[4 * offset as usize + 2] = stroke.blue;
+                        canvas[4 * offset as usize + 3] = 255
+                    }
+                }
+            },
+            svg::SvgEvent::Text(content) => text::render((width, height), &content, canvas.as_mut_slice())
         }
     }
 
@@ -43,3 +48,4 @@ pub fn render((width, height): (u64, u64), events: Vec<svg::SvgEvent>) -> Vec<u8
 }
 
 mod distance;
+mod text;
---
+++ src/text.rs
@@ -0,0 +1,47 @@
+extern crate rusttype;
+
+use self::rusttype::{FontCollection, Scale, point, PositionedGlyph};
+
+pub fn render((canvas_width, canvas_height): (u64, u64), content: &str, canvas: &mut [u8]) {
+    let font_data = include_bytes!("../Arial Unicode.ttf");
+    let collection = FontCollection::from_bytes(&font_data[..]);
+    let font = collection.into_font().unwrap(); // only succeeds if collection consists of one font
+
+    // Desired font pixel height
+    let height: f32 = 12.4; // to get 80 chars across (fits most terminals); adjust as desired
+    let _pixel_height = height.ceil() as usize;
+
+    // 2x scale in x direction to counter the aspect ratio of monospace characters.
+    let scale = Scale { x: height*2.0, y: height };
+
+    // The origin of a line of text is at the baseline (roughly where non-descending letters sit).
+    // We don't want to clip the text, so we shift it down with an offset when laying it out.
+    // v_metrics.ascent is the distance between the baseline and the highest edge of any glyph in
+    // the font. That's enough to guarantee that there's no clipping.
+    let v_metrics = font.v_metrics(scale);
+    let offset = point(0.0, v_metrics.ascent);
+
+    // Glyphs to draw for "RustType". Feel free to try other strings.
+    let glyphs: Vec<PositionedGlyph> = font.layout(content, scale, offset).collect();
+
+    // Find the most visually pleasing width to display
+    let _width = glyphs.iter().rev()
+        .filter_map(|g| g.pixel_bounding_box()
+                    .map(|b| b.min.x as f32 + g.unpositioned().h_metrics().advance_width))
+        .next().unwrap_or(0.0).ceil() as usize;
+
+    for g in glyphs {
+        if let Some(bb) = g.pixel_bounding_box() {
+            g.draw(|x, y, v| {
+                let x = x as i32 + bb.min.x;
+                let y = y as i32 + bb.min.y;
+                // There's still a possibility that the glyph clips the boundaries of the bitmap
+                if x >= 0 && x < canvas_width as i32 && y >= 0 && y < canvas_height as i32 {
+                    let x = x as usize;
+                    let y = y as usize;
+                    canvas[(x + y * canvas_width as usize)] = (v * 256.0) as u8;
+                }
+            })
+        }
+    }
+ }
